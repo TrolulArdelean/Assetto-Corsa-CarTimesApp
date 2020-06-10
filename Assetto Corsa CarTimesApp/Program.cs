78d2e1b9af60a22e9cef721c9f24f41ea3ce955f
Alin Soritau <alin_s230892@yahoo.com>
2020-06-10 19:59:53 +0300

    i made a commit

--- Assetto Corsa CarTimesApp/Program.cs
+++ Assetto Corsa CarTimesApp/Program.cs
@@ -11,9 +11,9 @@ namespace Assetto_Corsa_CarTimesApp
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            Console.WriteLine("miti");
         }
     }
 }
